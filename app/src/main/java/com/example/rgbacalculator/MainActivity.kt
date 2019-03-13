cbb610180d651467044d8bed332b126c3f5ab1fa
ugart <arthurugomes@edu.unifor.br>
2019-03-12 21:02:39 -0300

    configurações de tela

--- app/src/main/java/com/example/rgbacalculator/MainActivity.kt
+++ app/src/main/java/com/example/rgbacalculator/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.SpannableStringBuilder
 import android.text.TextWatcher
+import android.widget.ArrayAdapter
 import android.widget.SeekBar
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -53,6 +54,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main) //associando o layout criado à activity
+        spinnerPopulating()
         writingHexDecNumber()
         seekingEditTextNumber()
     }
@@ -65,6 +67,7 @@ class MainActivity : AppCompatActivity() {
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
 
                 if (!p0.isNullOrEmpty()) {
+
                     seekBar.setOnSeekBarChangeListener(null) //anulando o listener da seekbar enquanto escrevo no edittext
                     seekBar.progress = p0.toString().toInt() //recebendo no progress da seekbar o valor digitado no edittext
                     seekBar.setOnSeekBarChangeListener(seekbar1ChangeListener) //ativando o listener da seekbar novamente
@@ -147,6 +150,13 @@ class MainActivity : AppCompatActivity() {
     //---------------------------------------- Para mostrar as cores na tela -------------------------------\\
     fun showColor(){
 
+    }
+
+    //---------------------------------------- Populando o spinner com os tipos de cores -------------------------------\\
+    fun spinnerPopulating(){
+        val colorTypes = arrayOf("Decimal", "Hexadecimal")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, colorTypes)
+        spinner.adapter = adapter
     }
 
 }
