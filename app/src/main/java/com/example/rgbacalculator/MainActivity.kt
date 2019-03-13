b2e64e31bf7ce1b49b3b87398ae7bcea94361f76
ugart <arthurugomes@edu.unifor.br>
2019-03-13 20:55:28 -0300

    ajustes e configuracoes de cores

--- app/src/main/java/com/example/rgbacalculator/MainActivity.kt
+++ app/src/main/java/com/example/rgbacalculator/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.example.rgbacalculator
 
-import android.support.v7.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.text.Editable
+import android.text.InputType
 import android.text.SpannableStringBuilder
 import android.text.TextWatcher
-import android.widget.ArrayAdapter
+import android.view.View
 import android.widget.SeekBar
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
+import java.lang.Integer.parseInt
 
 class MainActivity : AppCompatActivity() {
 
@@ -54,14 +58,48 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main) //associando o layout criado à activity
-        spinnerPopulating()
-        writingHexDecNumber()
+        button_type.text = "Decimal"
+        writingDecNumber()
+        writingHexNumber()
         seekingEditTextNumber()
+
+        button_type.setOnClickListener {
+            buttonClick()
+        }
+
+
     }
 
-    fun writingHexDecNumber(){
+    fun writingHexNumber() {
+        editText_hex.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                p0.toString().replace("#", "")
+
+                try {
+                    if (!p0.isNullOrEmpty()) {
+                        color_show_space.setBackgroundColor(Color.parseColor(p0.toString()))
+                    }
+                } catch (e: Exception) {
+
+                }
+
+            }
+
+        })
+    }
+
+    fun writingDecNumber() {
+
         editText1.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(p0: Editable?) {}
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
@@ -71,6 +109,7 @@ class MainActivity : AppCompatActivity() {
                     seekBar.setOnSeekBarChangeListener(null) //anulando o listener da seekbar enquanto escrevo no edittext
                     seekBar.progress = p0.toString().toInt() //recebendo no progress da seekbar o valor digitado no edittext
                     seekBar.setOnSeekBarChangeListener(seekbar1ChangeListener) //ativando o listener da seekbar novamente
+                    color_show_space.setBackgroundColor(Color.argb(seekBar4.progress, seekBar.progress, seekBar2.progress, seekBar3.progress))
                 }
 
             }
@@ -83,9 +122,11 @@ class MainActivity : AppCompatActivity() {
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
 
                 if (!p0.isNullOrEmpty()) {
+
                     seekBar2.setOnSeekBarChangeListener(null)
                     seekBar2.progress = p0.toString().toInt()
                     seekBar2.setOnSeekBarChangeListener(seekbar2ChangeListener)
+                    color_show_space.setBackgroundColor(Color.argb(seekBar4.progress, seekBar.progress, seekBar2.progress, seekBar3.progress))
                 }
 
             }
@@ -98,9 +139,11 @@ class MainActivity : AppCompatActivity() {
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
 
                 if (!p0.isNullOrEmpty()) {
+
                     seekBar3.setOnSeekBarChangeListener(null)
                     seekBar3.progress = p0.toString().toInt()
                     seekBar3.setOnSeekBarChangeListener(seekbar3ChangeListener)
+                    color_show_space.setBackgroundColor(Color.argb(seekBar4.progress, seekBar.progress, seekBar2.progress, seekBar3.progress))
                 }
 
             }
@@ -113,9 +156,11 @@ class MainActivity : AppCompatActivity() {
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
 
                 if (!p0.isNullOrEmpty()) {
+
                     seekBar4.setOnSeekBarChangeListener(null)
                     seekBar4.progress = p0.toString().toInt()
                     seekBar4.setOnSeekBarChangeListener(seekbar4ChangeListener)
+                    color_show_space.setBackgroundColor(Color.argb(seekBar4.progress, seekBar.progress, seekBar2.progress, seekBar3.progress))
                 }
 
             }
@@ -124,7 +169,7 @@ class MainActivity : AppCompatActivity() {
 
     //---------------------------------------- Associando seekbar com editText -------------------------------\\
 
-    fun seekingEditTextNumber(){
+    fun seekingEditTextNumber() {
 
         seekBar.setOnSeekBarChangeListener(seekbar1ChangeListener)
 
@@ -136,27 +181,38 @@ class MainActivity : AppCompatActivity() {
     }
 
     //---------------------------------------- Convertendo as cores de Decimal para RGBA -------------------------------\\
-    fun decimalToRGBA(){
-
+    fun decimalToRGBA() {
+//        Color.argb()
     }
 
     //---------------------------------------- Convertendo as cores de Hexadecimal para RGBA -------------------------------\\
 
-    fun hexaToRGBA(){
-
-    }
-
 
     //---------------------------------------- Para mostrar as cores na tela -------------------------------\\
-    fun showColor(){
+    fun showColor() {
 
     }
 
     //---------------------------------------- Populando o spinner com os tipos de cores -------------------------------\\
-    fun spinnerPopulating(){
-        val colorTypes = arrayOf("Decimal", "Hexadecimal")
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, colorTypes)
-        spinner.adapter = adapter
+    fun showHide(view: View) {
+        view.visibility = if (view.visibility == View.VISIBLE) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+
+    fun buttonClick() {
+        if (button_type.text == "Decimal") {
+            showHide(constraintLayout_dec)
+            showHide(constraintLayout_hex)
+            button_type.text = "Hexadecimal"
+            button_type.inputType = InputType.TYPE_NUMBER_FLAG_DECIMAL
+        } else {
+            showHide(constraintLayout_hex)
+            showHide(constraintLayout_dec)
+            button_type.text = "Decimal"
+        }
     }
 
 }
